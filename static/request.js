2ed0ec82fb75ae2bb970cff8587250c8bf5f1b1f
dkrapiva <daryaipopova@yandex.ru>
2023-11-16 15:30:54 +0400

    добавлен TypeScript

--- static/request.js
+++ static/request.js
@@ -1,26 +1,23 @@
+import { createTable } from "./script.js";
 // getRequest(): посылает get-запрос для получения информации о директориях 
-function getRequest(root) {
-    let xhr = new XMLHttpRequest();
-    let host = window.location.href;
-    let url = host + `dir?root=${root}&sortValue=${sortValue}`;
+export function getRequest(root, sortValue) {
+    var xhr = new XMLHttpRequest();
+    var host = window.location.href;
+    var url = host + "dir?root=".concat(root, "&sortValue=").concat(sortValue);
     xhr.open('GET', url);
     xhr.responseType = 'json';
-
-    xhr.onerror = function (){
-        alert(`Сервер не отвечает ${xhr.status}: ${xhr.statusText}`);
-    }
-
+    xhr.onerror = function () {
+        alert("\u0421\u0435\u0440\u0432\u0435\u0440 \u043D\u0435 \u043E\u0442\u0432\u0435\u0447\u0430\u0435\u0442 ".concat(xhr.status, ": ").concat(xhr.statusText));
+    };
     xhr.onload = function () {
         callback(xhr);
     };
-
     xhr.send();
 }
-
 // callback(): вызывает функцию createTable после ответа от сервера
 function callback(xhr) {
-    let responseObj = xhr.response;
-    let fields = ["FileType", "Name", "Size"];
-    let tbody = document.querySelector('#tbd');
-    createTable(tbody, responseObj, fields); 
+    var responseObj = xhr.response;
+    var fields = ["FileType", "Name", "Size"];
+    var tbody = document.querySelector('#tbd');
+    createTable(tbody, responseObj, fields);
 }
