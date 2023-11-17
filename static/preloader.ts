a1b40c17788c8de25355fbbeccdb64b583982930
dkrapiva <daryaipopova@yandex.ru>
2023-11-17 12:34:31 +0400

    Добавлен прелоадер

---
+++ static/preloader.ts
@@ -0,0 +1,8 @@
+window.addEventListener('load', function() {
+    let preloader = document.getElementById('preloader');
+    preloader!.classList.add('hide-preloader');
+    setInterval(function() {
+          preloader!.classList.add('preloader-hidden');
+    }, 990);
+});
+
